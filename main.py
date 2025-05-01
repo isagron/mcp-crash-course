c0db882b12969af87335ca3c9ca49b74ee78299f
isagron <innon12310@gmail.com>
2025-05-01 20:55:49 +0300

    Add initial functionality to main.py, including LLM setup and StdioServerParameters. Update pyproject.toml to include isort dependency. Modify uv.lock to reflect new isort package. Minor formatting change in weather_server.py.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 load_dotenv()
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/isagron/sw_projects/learning/mcp-crash-course/servers/math_server.py"],
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
