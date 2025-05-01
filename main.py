20413b21ca29e864118b515ff0512db32ead5b20
isagron <innon12310@gmail.com>
2025-05-01 17:03:26 +0300

    Initial commit without sensitive data

---
+++ main.py
@@ -0,0 +1,12 @@
+import asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+async def main():
+    print("Hello from mcp-crash-course!")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
